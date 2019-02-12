f63fb7bdc76ea7318599fee19dfc1ac2db1851dd
Joe Andaluz <jandaluz1@gmail.com>
2019-02-11 21:12:07 -0500

    upgraded expo and installed nativebase for styling

--- Components/Home.js
+++ Components/Home.js
@@ -1,6 +1,30 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
+import { View, StyleSheet, TextInput } from 'react-native';
+import { Container, Content, H2, Input, Button, Text } from 'native-base';
 import quotes from '../quotes';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '95%'
+    // backgroundColor: '#333444'
+  },
+  // content: {
+  //   width: '95%'
+  // },
+  title: {
+    color: '#333444',
+    fontSize: 22,
+    justifyContent: 'center'
+  },
+  inputField: {
+    backgroundColor: '#aaaaaa',
+    // height: 1,
+    fontSize: 18
+  }
+});
 
 export default class Home extends Component {
   constructor(props) {
@@ -29,34 +53,36 @@ export default class Home extends Component {
   }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.title}>WHAT'S THAT FROM?</Text>
-        <TextInput
-          style={styles.inputField}
-          onChangeText={this.handleChange}
-          value={this.state.guess}
-          placeholder="Movie Quote Here"
-        />
-        <Button onPress={this.handleSubmit} title="Search for Quote" />
-      </View>
+      // <View style={styles.container}>
+      //   <Text style={styles.title}>WHAT'S THAT FROM?</Text>
+      //   <TextInput
+      //     style={styles.inputField}
+      //     onChangeText={this.handleChange}
+      //     value={this.state.guess}
+      //     placeholder="Movie Quote Here"
+      //   />
+      //   <Button onPress={this.handleSubmit} title="Search for Quote" />
+      // </View>
+      <Container style={styles.container}>
+        <Content style={styles.content}>
+          <H2 style={styles.title}>What's That From</H2>
+          <Input
+            full
+            style={styles.inputField}
+            onChangeText={this.handleChange}
+            value={this.state.guess}
+          />
+          <Button
+            rounded
+            info
+            onPress={this.handleSubmit}
+            full
+            disabled={this.state.guess.length < 1}
+          >
+            <Text>Search</Text>
+          </Button>
+        </Content>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-    // backgroundColor: '#333444'
-  },
-  title: {
-    color: '#333444',
-    fontSize: 22
-  },
-  inputField: {
-    // backgroundColor: '#aaaaaa',
-    height: 20,
-    fontSize: 18
-  }
-});
